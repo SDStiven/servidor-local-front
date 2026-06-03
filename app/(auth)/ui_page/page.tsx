53b901fc8ca7a4d99979a574693281513c3984ce
stiven <diasstiven2@gmail.com>
2026-06-03 10:29:13 -0100

    feat: create proposal dashboard page with GraphQL integration to fetch and display user proposals

--- app/(auth)/ui_page/page.tsx
+++ app/(auth)/ui_page/page.tsx
@@ -4,53 +4,51 @@ import { Proposal_Board } from "@/components/components_project/proposal_board";
 import { gql } from "@apollo/client";
 import { useQuery } from "@apollo/client/react";
 
-export default function ProposalPage() {
-    interface PropostaRequest{
-        id: string,
-        owner: string,
-        estado: string,
-        id_prestacao_servico:{
-            id_servico:{
-                nome: string,
-                categoria:{
-                    id: string
-                    icone: string
-                }
-            }
-        }
-    }
-
-    const dataQuery = gql`
-        query GetAllProposta {
-            getAllProposta {
-                id
-                owner
-                estado
-                id_prestacao_servico{
-                    id_servico{
-                        nome
-                        categoria{
-                            id
-                            icone
-                        }
-                    }
-                }
-            }
-        }`
-
-
-    function getAllProposta(): any {    
-        const { data, loading, error } = useQuery<PropostaRequest>(dataQuery);
-        return {loading, error, data };
+interface PropostaRequest {
+  id: string;
+  owner: string;
+  estado: string;
+  id_prestacao_servico: {
+    id_servico: {
+      nome: string;
+      categoria: {
+        id: string;
+        icone: string;
+      };
     };
+  };
+}
 
+interface GetAllPropostaData {
+  getAllProposta: PropostaRequest[];
+}
 
+const dataQuery = gql`
+  query GetAllProposta {
+    getAllProposta {
+      id
+      owner
+      estado
+      id_prestacao_servico {
+        id_servico {
+          nome
+          categoria {
+            id
+            icone
+          }
+        }
+      }
+    }
+  }
+`;
 
-    const { loading, error, data } = getAllProposta();
-    if (loading) return <p>Loading...</p>;
-    if (error) return <p>Error: {error.message}</p>;
+export default function ProposalPage() {
+  const { loading, error, data } = useQuery<GetAllPropostaData>(dataQuery);
 
-    const propostas = data?.getAllProposta || []; 
+  if (loading) return <p>Loading...</p>;
+  if (error) return <p>Error: {error.message}</p>;
+
+  const propostas = data?.getAllProposta || []; 
 
     return (
     <div>
